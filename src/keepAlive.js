65429a03878357d7305986cca295683361102c1c
Yandond <yannickdonders@hotmail.be>
2024-03-22 00:49:01 +0100

    fix mistake

--- src/keepAlive.js
+++ src/keepAlive.js
@@ -5,7 +5,8 @@ server.all('/', (req, res) => {
   res.send('Result: [OK]')
 })
 
-mmodule.exports = () =>
+
+module.exports = () =>
  {
     server.listen(3000, () => {
     console.log('server is now ready!')
